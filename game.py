49bf61ea74253cfd2a77eed648d242f7050c84ee
AntonOlenchenko1 <anton.olenchenko@e-u.edu.ua>
2023-11-26 18:45:12 +0200

    S.O.L.I.D

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 import PySimpleGUI as sg
 import random
 
-def play_guess_the_number(difficulty):
-    def generate_new_secret_number(min_value, max_value):
+class NumberGame:
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.min_value, self.max_value = self.get_difficulty_range(self.difficulty)
+        self.secret_number = self.generate_new_secret_number(self.min_value, self.max_value)
+
+    def generate_new_secret_number(self, min_value, max_value):
         return random.randint(min_value, max_value)
 
-    def get_difficulty_range(difficulty):
+    def get_difficulty_range(self, difficulty):
         if difficulty == "Легкий":
             return 1, 50
         elif difficulty == "Середній":
@@ -15,47 +20,55 @@ def play_guess_the_number(difficulty):
         else:
             raise ValueError("Непідтримуваний рівень складності")
 
-    layout = [
-        [sg.Text("Виберіть рівень складності:"), sg.Combo(["Легкий", "Середній", "Важкий"], default_value=difficulty, key="difficulty")],
-        [sg.Button("Почати нову гру")],
-        [sg.Text("Вгадайте число:"), sg.InputText(key="input_guess"), sg.Button("Вгадати")],
-        [sg.Text("", size=(30, 1), key="output_message")]
-    ]
+    def play(self):
+        layout = [
+            [sg.Text("Виберіть рівень складності:"), sg.Combo(["Легкий", "Середній", "Важкий"], default_value=self.difficulty, key="difficulty")],
+            [sg.Button("Почати нову гру")],
+            [sg.Text("Вгадайте число:"), sg.InputText(key="input_guess"), sg.Button("Вгадати")],
+            [sg.Text("", size=(30, 1), key="output_message")]
+        ]
 
-    window = sg.Window("Гра 'Вгадай число'", layout, finalize=True)
+        window = sg.Window("Гра 'Вгадай число'", layout, finalize=True)
 
-    while True:
-        event, values = window.read()
+        while True:
+            event, values = window.read()
 
-        if event == sg.WIN_CLOSED:
-            break
+            if event == sg.WIN_CLOSED:
+                break
 
-        if event == "Почати нову гру":
-            difficulty = values["difficulty"]
-            min_value, max_value = get_difficulty_range(difficulty)
-            secret_number = generate_new_secret_number(min_value, max_value)
-            window["output_message"].update("")
-            continue
-
-        if event == "Вгадати":
-            try:
-                user_guess = int(values["input_guess"])
-            except ValueError:
-                window["output_message"].update("Введіть коректне число.")
+            if event == "Почати нову гру":
+                self.difficulty = values["difficulty"]
+                self.min_value, self.max_value = self.get_difficulty_range(self.difficulty)
+                self.secret_number = self.generate_new_secret_number(self.min_value, self.max_value)
+                window["output_message"].update("")
                 continue
 
-            if min_value <= user_guess <= max_value:
-                window["output_message"].update("")
-                if user_guess < secret_number:
-                    window["output_message"].update("Загадане число більше. Спробуйте ще раз.")
-                elif user_guess > secret_number:
-                    window["output_message"].update("Загадане число менше. Спробуйте ще раз.")
-                else:
-                    sg.popup_ok(f"Вітаємо! Ви вгадали число {secret_number}.")
-            else:
-                window["output_message"].update(f"Введене число має бути в діапазоні від {min_value} до {max_value}.")
+            if event == "Вгадати":
+                self.handle_guess(window, values)
 
-    window.close()
+        window.close()
+
+    def handle_guess(self, window, values):
+        try:
+            user_guess = int(values["input_guess"])
+        except ValueError:
+            window["output_message"].update("Введіть коректне число.")
+            return
+
+        if self.min_value <= user_guess <= self.max_value:
+            window["output_message"].update("")
+            self.check_guess(window, user_guess)
+        else:
+            window["output_message"].update(f"Введене число має бути в діапазоні від {self.min_value} до {self.max_value}.")
+
+    def check_guess(self, window, user_guess):
+        if user_guess < self.secret_number:
+            window["output_message"].update("Загадане число більше. Спробуйте ще раз.")
+        elif user_guess > self.secret_number:
+            window["output_message"].update("Загадане число менше. Спробуйте ще раз.")
+        else:
+            sg.popup_ok(f"Вітаємо! Ви вгадали число {self.secret_number}.")
 
 if __name__ == "__main__":
-    play_guess_the_number("Середній")
+    game = NumberGame("Середній")
+    game.play()
